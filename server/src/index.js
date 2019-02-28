6d388bc5dba7c086e8b76b7f2d74b566acc1ad4e
Elijah Liedtke <chimera.zen@gmail.com>
2019-02-28 17:56:18 -0500

    Connect DB, Schema, Resolvers, Models, Context, token & serverlessHTTP

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,66 @@
-import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
+import { AuthenticationError } from "apollo-server"
+import http from 'http'
 
+import express from 'express'
 import cors from 'cors'
+
+import db from './config/db'
+
+import schema from './schema'
+import resolvers from './resolvers'
+import models from './models'
+
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const port = 8000
 
 app.use(cors())
 
-const schema = {}
-const resolvers = {}
+const getMe = async req => {
+  const token = req.headers["x-token"]
+
+  if (token) {
+    try {
+      return await jwt.verify(token, process.env.SECRET)
+    } catch (e) {
+      throw new AuthenticationError("Your session expired. Sign in again.")
+    }
+  }
+}
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  path: "/graphql",
+  context: async ({ req, connection }) => {
+    if(connection) {
+      return {
+        models,
+      }
+    }
+    
+    if (req) {
+      const me = await getMe(req)
+      
+      return {
+        models,
+        me,
+        secret: process.env.SECRET,
+      }
+    }
+  }
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
 
-app.listen({ port }, () => {
-  console.log(`Apollo Server on http://localhost:${port}/graphql`)
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
+db.connect().then(() => {
+  httpServer.listen({ port }, () => {
+    console.log(`Server: ACTIVE`)
+  })
 })
