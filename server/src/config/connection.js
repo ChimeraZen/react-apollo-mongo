c768e8e1149962369b068c3038bd35181563acb3
Elijah Liedtke <chimera.zen@gmail.com>
2019-02-28 17:58:11 -0500

    Initialize TEST_DATABASE

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -7,9 +7,6 @@ const password = process.env.DB_PASS
 const hostname = process.env.DB_HOSTNAME
 const database = process.env.DATABASE
 
-/*
 module.exports = process.env.TEST_DATABASE
   ? `mongodb://localhost:27017/${process.env.TEST_DATABASE}`
   : `${host}${username}:${password}@${hostname}/${database}`
-  */
-module.exports = `mongodb://localhost:27017/ram-test`
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,13 +2,16 @@ import mongoose from 'mongoose'
 import dbURI from './connection'
 
 const connect = async () => {
+  console.log(`Server: CONNECTING...`)
   try {
     await mongoose.connect(dbURI, {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true
     })
+    console.log(`Server: CONNECTION SUCCESSFUL!`)
   } catch(err) {
+    console.log(`Server: !CONNECTION FAILURE!`)
     throw new Error(err)
   }
 }
