a7d0e0e3216c13653fd65d76231bda2410a0a275
Elijah Liedtke <chimera.zen@gmail.com>
2019-03-01 00:56:50 -0500

    Returned user mutation to pure function

--- server/src/resolvers/user/mutation.js
+++ server/src/resolvers/user/mutation.js
@@ -1,63 +1,26 @@
-import { AuthenticationError, UserInputError } from 'apollo-server'
 import { combineResolvers } from 'graphql-resolvers'
-import { isAdmin, createToken } from '../../validation'
+import { isAdmin } from '../../validation'
 
 module.exports = {
   signUp: async (
     parent,
     { username, email, password, role },
-    { models, secret },
+    { models, secret }
   ) => {
-    const userExists = await models.User.findByLogin(username)
-
-    if (!userExists) {
-    const user = await models.User.create({
-        username,
-        email,
-        role,
-        password: await models.User.generatePasswordHash(password),
-      })
-
-      return { token: createToken(user, secret, '30m') }
-    } else {
-      throw new UserInputError(
-        'User already exists',
-      )
-    }
+    return await models.User.signUp({
+      username,
+      email,
+      role,
+      password
+    }, secret)
   },
 
   signIn: async (
     parent,
     { login, password },
-    { models, secret },
+    { models, secret }
   ) => {
-    if (!login) {
-      throw new UserInputError(
-        'A username or email is required'
-      )
-    }
-    if (!password) {
-      throw new UserInputError(
-        'A password is required'
-      )
-    }
-
-    const user = await models.User.findByLogin(login)
-
-    if (!user) {
-      throw new UserInputError(
-        'No user found with these credentials',
-      )
-    }
-
-    // eventually need to remove password from model before being returned and store the correction as a static in the model schema
-    const isValid = await models.User.validatePassword(password, user.password)
-
-    if (!isValid) {
-      throw new AuthenticationError('Invalid password.')
-    }
-
-    return { token: createToken(user, secret, '30m') }
+    return await models.User.signIn(login, password, secret)
   },
 
   deleteUser: combineResolvers(
@@ -68,6 +31,6 @@ module.exports = {
       return result
         ? true
         : false
-    },
+    }
   ),
 }
