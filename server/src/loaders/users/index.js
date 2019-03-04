4d1c933d6df8489fc64960636f55f96b5fc20033
Elijah Liedtke <chimera.zen@gmail.com>
2019-03-03 20:18:51 -0500

    Removed console.log

--- server/src/loaders/users/index.js
+++ server/src/loaders/users/index.js
@@ -3,6 +3,5 @@ module.exports.batchUsers = async (keys, models) => {
     .where('_id')
     .in(keys)
   
-console.log('batchUsers', users, keys)
   return keys.map(key => users.find(user => user.id === key))
 }
